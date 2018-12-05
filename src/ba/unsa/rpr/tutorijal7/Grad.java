0795c5586a7e2d12e70e96ded143e00feb1c3cb1
jusufdelalic <jdelalic1@etf.unsa.ba>
2018-12-05 17:51:13 +0100

    Initial commit

---
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -0,0 +1,42 @@
+package ba.unsa.rpr.tutorijal7;
+
+public class Grad {
+
+    private String naziv;
+    private int brojStanovnika;
+    private double [] temperature;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+
+
+
+    public Grad() {
+        this.naziv = null;
+        this. brojStanovnika = 0;
+        temperature = null;
+    }
+
+
+}
---
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -0,0 +1,22 @@
+package ba.unsa.rpr.tutorijal7;
+
+import java.util.ArrayList;
+
+public class UN {
+
+    private ArrayList<Drzava> drzave;
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+
+    public UN() {
+
+        drzave = null;
+
+    }
+}
