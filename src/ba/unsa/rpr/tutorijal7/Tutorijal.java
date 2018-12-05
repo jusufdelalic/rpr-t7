aaaac3c5fd7b8774f5733a122b41d9934ec48b43
jusufdelalic <jdelalic1@etf.unsa.ba>
2018-12-05 19:12:50 +0100

    Zapoceo ucitajGradove

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,8 +1,85 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
 
+    public static ArrayList<Grad> ucitajGradove() throws FileNotFoundException {
+
+        ArrayList<Grad> gradovi = new ArrayList<Grad>();
+
+        Scanner ulaz; // omotač za tekstualni ulazni tok
+
+        // Konstruisanje ulaznog toka za datoteku mjerenja.txt
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        }
+
+        catch(FileNotFoundException e) {
+            System.out.println("Datoteka mjerenja.txt ne postoji ili se ne može otvoriti.");
+            System.out.println("Greška: " + e);
+            throw e; // kraj programa
+        }
+
+
+        try {
+            // Učitavamo podatke...
+
+            while (ulaz.hasNext()) {
+
+                String s = ulaz.nextLine();
+                String[] podatak = s.split(","); // razdvajanje stringova preko ","
+
+                //if(!(podatak[0].charAt(0)>='A' && podatak[0].charAt(0)<='Z')) throw nelegalan naziv grada...
+
+
+                Grad grad = new Grad();
+
+                grad.setNaziv(podatak[0]);
+
+                double[] temperature = new double [1000];
+
+                int i = 0;
+
+                while( i < 1000 && i < podatak.length - 1 ) {
+
+                    //if( temperature[i] < -273.15 ) throw nelegalna temperatura...
+
+                    temperature[i] = Double.parseDouble(podatak[i+1]);
+
+                    i++;
+                }
+
+                grad.setTemperature(temperature);
+
+                gradovi.add(grad);
+
+            }
+
+        }
+
+        catch(Exception e) {
+            System.out.println("Problem pri čitanju podataka.");
+            System.out.println("Greška: " + e);
+        }
+
+
+
+
+        return  gradovi;
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            ucitajGradove();
+        }
+
+        catch(FileNotFoundException e) {}
+
     }
 }
